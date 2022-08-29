1e8f1f134a862dce898c5cca0bf60a41107c2599
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2022-08-29 19:09:56 -0400

    Initial commit

---
+++ app/src/main/java/net/pldyn/mtgdeckwalker/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package net.pldyn.mtgdeckwalker.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
